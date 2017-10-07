1fe127eb193979f71263962089ba43b51b558690
MrTurtles <mineshaftdave2@gmail.com>
2017-10-07 11:33:06 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,8 +4,8 @@ exports.run = (client, message) => {
   const embed2 = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
-    .addField('With Permissions', 'https://discordapp.com/oauth2/authorize?permissions=2146958591&scope=bot&client_id=304258486466904064')
-    .addField('Without Permissions', 'https://discordapp.com/oauth2/authorize?client_id=304258486466904064&scope=bot');
+    .addField('Give to your friends!', 'https://discord.gg/FwzDcKS')
+    //.addField('Without Permissions', 'https://discordapp.com/oauth2/authorize?client_id=304258486466904064&scope=bot');
   message.channel.sendEmbed(embed2)
 };
 
