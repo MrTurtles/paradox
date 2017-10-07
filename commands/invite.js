a619944dabf357999655766a3769ba54843d9e02
MrTurtles <mineshaftdave2@gmail.com>
2017-10-07 14:23:19 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,6 +5,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .addField('Give to your friends!', 'https://discord.gg/FwzDcKS')
+    .setThumbnail(client.user.avatarURL)
     //.addField('Without Permissions', 'https://discordapp.com/oauth2/authorize?client_id=304258486466904064&scope=bot');
   message.channel.sendEmbed(embed2)
 };
@@ -19,6 +20,6 @@ exports.conf = {
 exports.help = {
   name: 'invite',
   rank: 'Member',
-  description: '(MEMBER) - Creates link to invite this bot to your server.',
+  description: '(MEMBER) - Gives invite link to this server.',
   usage: 'invite'
 };
