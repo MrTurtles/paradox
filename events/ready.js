1367402d80c8c346ba83076ecd5d0d0973662d04
MrTurtles <mineshaftdave2@gmail.com>
2017-10-08 12:45:36 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 //const client = new Discord.Client();
 module.exports = client => {
   console.log(chalk.green('I\'m Online'));
-  let login = ',buy | Off Sale!'
+  let login = ',buy | On Sale!'
   client.user.setGame(login);
   client.user.setStatus('online');
   console.log(chalk.green(login));
