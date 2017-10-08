54138a318ec348ca5495fd75599c53a132a6759d
MrTurtles <mineshaftdave2@gmail.com>
2017-10-08 15:28:54 +0200

    Update strawpoll.js

--- commands/strawpoll.js
+++ commands/strawpoll.js
@@ -1,1 +1,19 @@
-e
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+    
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['ball', '8'],
+  permLevel: 1
+};
+
+exports.help = {
+  name: 'strawpoll',
+  rank: 'Moderator',
+  description: '(Moderator) - Returns messages like Yes, No, etc.',
+  usage: 'strawpoll [text]'
+};
+
