32f224d8ca70ed7e8d389b8d3fbf3a4ed4ec6e0a
MrTurtles <mineshaftdave2@gmail.com>
2017-10-08 15:51:55 +0200

    Update strawpoll.js

--- commands/strawpoll.js
+++ commands/strawpoll.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
+    message.delete();
     let question = args.join(' ');
     let user = message.author.username
     if (!question) return message.channel.sendEmbed(
